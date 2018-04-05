e242aa48e3df682ab0e375278a54b41f4343d2d5
a1705558 <37823220+a1705558@users.noreply.github.com>
2018-04-06 00:46:06 +0930

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,13 @@
+function log()
+{
+	document.getElementById("sign").style.display = "none";
+	document.getElementById("log").style.display = "block";
+	document.getElementById("account").style.display = "none";
+}
+
+function sign()
+{
+	document.getElementById("sign").style.display = "block";
+	document.getElementById("log").style.display = "none";
+	document.getElementById("account").style.display = "none";
+}
