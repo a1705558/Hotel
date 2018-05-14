750c73952648e0387afe9ac3a31011e6a48bdc7b
BrycePolley <30710310+BrycePolley@users.noreply.github.com>
2018-05-14 17:22:14 +0930

    Added some commands to fetch google ids so it can say #persons name# is signed in
    There is a list of commands and crap here:
    
    https://developers.google.com/identity/sign-in/web/build-button?authuser=1

--- public/hotel.js
+++ public/hotel.js
@@ -237,3 +237,44 @@ function initMap(x) {
    	});
     infowindow.open(map,marker);
 }
+
+//Google crap
+
+function onSuccess(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    		      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+
+    gapi.client.load('plus', 'v1', function () {
+        var request = gapi.client.plus.people.get({
+            'userId': 'me'
+        });
+        //Display the user details
+        request.execute(function (resp) {
+            var profileHTML = '<div class="profile"><div class="head">Welcome '+resp.name.givenName+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></div>';
+            profileHTML += '<img src="'+resp.image.url+'"/><div class="proDetails"><p>'+resp.displayName+'</p><p>'+resp.emails[0].value+'</p><p>'+resp.gender+'</p><p>'+resp.id+'</p><p><a href="'+resp.url+'">View Google+ Profile</a></p></div></div>';
+            $('.userContent').html(profileHTML);
+            $('#gSignIn').slideUp('slow');
+        });
+    });
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('gSignIn', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        $('.userContent').html('');
+        $('#gSignIn').slideDown('slow');
+    });
+}
